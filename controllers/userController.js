6378183c7d59dbccf10491eac2c2191967929c2e
developer <developer@pr-D204-Intern1.cencol.ca>
2017-07-17 17:15:27 -0400

    controllers and login functionality added

---
+++ controllers/userController.js
@@ -0,0 +1,48 @@
+var fetch = require('node-fetch');
+// login function 
+module.exports.signIn = (req, res) => {
+    var username = req.body.email;
+    var password = req.body.password;
+    console.log("username >> " + username + " >> " + password);
+    var auth = 'Basic ' + new Buffer(username + ':' + password).toString('base64'); // create authorization string
+    console.log('auth >>> ' + auth);
+    fetch('http://wetraqapi.azurewebsites.net/login', {
+        method: 'GET',
+        headers: {
+            Authorization: auth
+        }
+
+    }).then(function(response) {
+
+        return response.json();
+    }).then(function(json) {
+        var jsonResponse = (json);
+
+        if (jsonResponse.hasOwnProperty('user')) {
+            return res.render('./dashboard', {
+                title: 'Dashboard',
+                jsonResponse: jsonResponse,
+                email: jsonResponse.user.primary_email
+            });
+        } else {
+            return res.render('./error', {
+                title: "error",
+                message: jsonResponse.error.msg
+            });
+        }
+    }).catch(function(error) {
+        return res.render('./error', {
+            title: "error",
+            message: error
+        });
+    })
+}
+
+
+module.exports.displaySIgnInPage = (req, res) => {
+
+    return res.render('./dashboard', {
+        title: 'Login'
+
+    })
+}
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
+// require the users controller
+let usersController = require('../controllers/userController');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+    res.send('respond with a resource');
+}).post('/', (req, res, next) => {
+    usersController.signIn(req, res, next);
+});;
+
 
 module.exports = router;
