76f3db24f139b1ba2855233f7e2a8493abddff50
developer <developer@pr-D204-Intern1.cencol.ca>
2017-07-20 18:24:33 -0400

    register page added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,53 @@
 var fetch = require('node-fetch');
+
+// render register page
+module.exports.showRegisterPage = (req, res) => {
+    return res.render('auth/register', {
+        title: "Register",
+
+    });
+}
+
+
+//  register post api
+module.exports.register = (req, res) => {
+    var username = req.body.email;
+    var password = req.body.password;
+    console.log("username >> " + username + " >> " + password);
+    var passwordString = 'Basic ' + new Buffer(password).toString('base64'); // create authorization string
+    console.log('auth >>> ' + auth);
+    fetch('http://wetraqapi.azurewebsites.net/register', {
+        method: 'POST',
+        headers: {
+            Authorization: auth
+        }
+
+    }).then(function(response) {
+
+        return response.json();
+    }).then(function(json) {
+        var jsonResponse = (json);
+
+        if (jsonResponse.hasOwnProperty('user')) {
+            return res.render('./dashboard_user', {
+                title: 'Dashboard',
+                jsonResponse: jsonResponse,
+                email: jsonResponse.user.primary_email
+            });
+        } else {
+            return res.render('./error', {
+                title: "error",
+                message: jsonResponse.error.msg
+            });
+        }
+    }).catch(function(error) {
+        return res.render('./error', {
+            title: "error",
+            message: error
+        });
+    })
+}
+
 // login function 
 module.exports.signIn = (req, res) => {
     var username = req.body.email;
--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,18 @@ var express = require('express');
 var router = express.Router();
 // require the users controller
 let usersController = require('../controllers/userController');
-/* GET users listing. */
+/*  users sign in. */
 router.get('/', function(req, res, next) {
     res.send('respond with a resource');
 }).post('/', (req, res, next) => {
     usersController.signIn(req, res, next);
+});
+
+/*  users sign in. */
+router.get('/register', function(req, res, next) {
+    usersController.showRegisterPage(req, res, next);
+}).post('/register', (req, res, next) => {
+    usersController.register(req, res, next);
 });
 
 
