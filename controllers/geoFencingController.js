6c89683cc619fe3965799bec81364a8e0aafdb9f
developer <developer@pr-D204-Intern1.cencol.ca>
2017-08-22 17:27:56 -0400

    map pin working

--- controllers/geoFencingController.js
+++ controllers/geoFencingController.js
@@ -20,7 +20,7 @@ module.exports.DisplayPage = (req, res) => {
 module.exports.UpdateBulkGeoFencing = (req, res) => {
     var session = req.session;
     console.log('on updatre method geo fence ');
-    var radius = req.body.radius;
+    var radius = req.body.seekbar_val;
     var devicearray = req.body.deviceids_json;
     var lat = req.body.lat;
     var longi = req.body.longi;
@@ -32,41 +32,41 @@ module.exports.UpdateBulkGeoFencing = (req, res) => {
     console.log(radius + " :: " + devicearray + " :: " + lat + "  :: " + longi);
 
     // update device schedule
-    fetch('http://api.wetraq.ca/device/geofence', {
-        method: 'PATCH',
-        headers: {
-            'Content-Type': 'application/json',
-            'cookie': session.c
-        },
-        credentials: 'same-origin',
-        body: '{ "device":         { "geofencing": true, "geofence_radius":' + radius + ', "geofence_latitude": 43.890975,          "geofence_longitude": -79.23227 },          "device_list": ["00000000000000000003", "00000000000000000002"] }'
-            // body: '{' +
-            //     '"device": {' +
-            //     '"geofencing": true,' +
-            //     '"geofence_radius": ' + radius + "," +
-            //     '"geofence_latitude":' + lat + "," +
-            //     '"geofence_longitude":' + longi +
-            //     '},' +
-            //     '"device_list": ' + devicearray +
-            //     '}'
-    }).then(function(response) {
-        var responseStatus = response.status;
-        console.log("response status >> " + "   " + session.c + "  " + response.status + "   " + response.message);
-        if (responseStatus == 200)
+    // fetch('http://api.wetraq.ca/device/geofence', {
+    //     method: 'PATCH',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //         'cookie': session.c
+    //     },
+    //     credentials: 'same-origin',
+    //     body: '{ "device":         { "geofencing": true, "geofence_radius":' + radius + ', "geofence_latitude": 43.890975,          "geofence_longitude": -79.23227 },          "device_list": ["00000000000000000003", "00000000000000000002"] }'
+    //         // body: '{' +
+    //         //     '"device": {' +
+    //         //     '"geofencing": true,' +
+    //         //     '"geofence_radius": ' + radius + "," +
+    //         //     '"geofence_latitude":' + lat + "," +
+    //         //     '"geofence_longitude":' + longi +
+    //         //     '},' +
+    //         //     '"device_list": ' + devicearray +
+    //         //     '}'
+    // }).then(function(response) {
+    //     var responseStatus = response.status;
+    //     console.log("response status >> " + "   " + session.c + "  " + response.status + "   " + response.message);
+    //     if (responseStatus == 200)
 
-            return res.redirect('/geofence');
+    //         return res.redirect('/geofence');
 
-        else {
-            return res.render('./', {
-                title: "update failed",
-                message: 'Update Failed on Geofence'
-            });
-        }
+    //     else {
+    //         return res.render('./', {
+    //             title: "update failed",
+    //             message: 'Update Failed on Geofence'
+    //         });
+    //     }
 
-    }).catch(function(error) {
-        return res.render('./error', {
-            title: "error",
-            message: error
-        });
-    });
+    // }).catch(function(error) {
+    //     return res.render('./error', {
+    //         title: "error",
+    //         message: error
+    //     });
+    // });
 }
