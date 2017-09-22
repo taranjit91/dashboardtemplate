937b65e26e0df18c82417e1520230a1c50a52fb9
Taranjit Kaur <tkaur34@my.centennialcollege.ca>
2017-09-22 15:15:45 -0400

    geofence and nav bar updated

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -14,51 +14,6 @@ module.exports.Display = (req, res) => {
     }
 }
 
-// find the device by id and populate the form
-module.exports.UpdateSchedule = (req, res) => {
-
-    var session = req.session;
-    if (session.c) {
-        console.log("user logged in" + session.username);
-        console.log('on updatre method &&& ' + session.c);
-        // find one game by its id
-        fetch('http://api.wetraq.ca/device/' + '00000000000000000002' + '/schedule', {
-            method: 'PATCH',
-            headers: {
-                'Content-Type': 'application/json',
-                'cookie': usersController.cookieValue()
-            },
-            credentials: 'same-origin',
-            body: '{"device": {	"is_reporting": true,"reporting_days": [true, true, true, true, true, true, true],	"reporting_start": 0,	"reporting_end": 86400,	"reporting_freq": 600,"reporting_offset": -14400,"next_report": 1502281440	}}'
-        }).then(function(response) {
-            var responseStatus = response.status;
-            if (responseStatus == 204)
-
-                return res.render('./device/deviceDetails', {
-                title: 'Edit Device',
-
-
-            });
-            else {
-                return res.render('./schedule', {
-                    title: "update failed",
-                    message: 'Update Failed on devices'
-                });
-            }
-
-        }).catch(function(error) {
-            return res.render('./error', {
-                title: "error",
-                message: error
-            });
-        });
-    } else {
-        console.log("user not logged in");
-    }
-
-
-    // 
-}
 
 
 // updating more devices
@@ -69,13 +24,18 @@ module.exports.UpdateBulkSchedule = (req, res) => {
     var devicearray = req.body.deviceids_json;
     var freq = req.body.freq;
     var picker1 = req.body.timepicker1;
-    picker1 = picker1.replace(":", ".");
-    var startTime = parseFloat(picker1) * 60 * 60;
+    var startTime, endTime;
+    if (picker1 != undefined) {
+        picker1 = picker1.replace(":", ".");
+
+        startTime = parseFloat(picker1) * 60 * 60;
+    }
 
     var picker2 = req.body.timepicker2;
-    picker2 = picker2.replace(":", ".");
-    var endTime = parseFloat(picker2) * 60 * 60;
-
+    if (picker2 != undefined) {
+        picker2 = picker2.replace(":", ".");
+        endTime = parseFloat(picker2) * 60 * 60;
+    }
 
     freq = freq * 60 * 60; // converting value to seconds
 
